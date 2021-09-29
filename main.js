f0fee73d723b8e7d88df718ae48b43f9622ac9d2
kpym <kpym.tzanev@gmail.com>
2021-09-29 15:14:16 +0200

    passage aux getter/setter

--- main.js
+++ main.js
@@ -5,49 +5,54 @@ document.addEventListener('alpine:init', () => {
 })
 
 RIO = {
-  tel:       '',
-  rio:       '',
   oo:        '',
   q:         '',
   rrrrrr:    '',
   ccc:       '',
 
-  normalisePhone(){
+  // getter/setter pour tel
+  telnum:    '',
+  get tel(){
+    return this.telnum
+  },
+  set tel(newPhone){
     // On normalise le numéro de téléphone pour être de la forme 0XXXXXXXXX.
-    this.tel = this.tel.replace(/[^0-9+]/g, '').replace(/\+330?/g, '0').substr(0,10)
+    this.telnum = newPhone.replace(/[^0-9+]/g, '').replace(/\+330?/g, '0').substr(0,10)
   },
-  toRIO(){
-    this.rio = this.oo + this.q + this.rrrrrr + this.ccc;
+  // getter/setter pour rio
+  get rio(){
+    return  this.oo + this.q + this.rrrrrr + this.ccc;
   },
-  fromRIO(){
+  set rio(newRIO) {
     // On normalise le RIO
-    this.rio = this.rio.toUpperCase().replace(/\s/g, '').substr(0,12);
+    newRIO = newRIO.toUpperCase().replace(/\s/g, '').substr(0,12);
 
-    this.oo = this.rio.substr(0,2);
-    this.q = this.rio.substr(2,1);
-    this.rrrrrr = this.rio.substr(3,6);
-    this.ccc = this.rio.substr(9,3);
+    this.oo = newRIO.substr(0,2);
+    this.q = newRIO.substr(2,1);
+    this.rrrrrr = newRIO.substr(3,6);
+    this.ccc = newRIO.substr(9,3);
   },
-  okTel() {
+  // les valaurs calculées
+  get okTel() {
     return /^[0-9]{10}$/.test(this.tel);
   },
-  okRio() {
+  get okRio() {
     return /^[A-Z0-9+]{12}$/.test(this.rio);
   },
-  operateur() {
-    if (this.oo) {
-      return operateurs.filter(o => o.prefix == this.oo).map(o => o.nom).join(", ") || "[inconnu]";
+  get operateur() {
+    if (!this.oo) {
+      return "";
     }
-    return "";
+    return operateurs.filter(o => o.prefix == this.oo).map(o => o.nom).join(", ") || "[inconnu]";
   },
-  type() {
+  get type() {
     // on pourrait aussi vérifier si oo[0] > "E"
-    if (this.oo) {
-      return operateurs.filter(o => o.prefix == this.oo).map(o => o.type)[0] || "[inconnu]";
+    if (!this.oo) {
+      return "";
     }
-    return "";
+    return operateurs.filter(o => o.prefix == this.oo).map(o => o.type)[0] || "[inconnu]";
   },
-  qtype() {
+  get qtype() {
     if (!this.q) {
       return "";
     }
@@ -60,20 +65,20 @@ RIO = {
         return "[inconnu]";
     }
   },
-  riotel() {
+  get riotel() {
     if (this.rio.length < 9 || this.tel.length != 10) {
       return "";
     }
     return this.rio.substr(0,9) + this.tel;
   },
-  abc() {
+  get abc() {
     ordre = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+"; // caractères utilisés pour le codage (37 différents)
 
     if (this.tel.length != 10 || this.rio.length < 9) {
       return "";
     }
     a = b = c = 0 // initialisation de a, b et c
-    for (const ch of this.riotel()) {
+    for (const ch of this.riotel) {
         position  = ordre.indexOf(ch);
         a = (1 * a + position) % 37;
         b = (2 * b + position) % 37;
